b2411daf218ebed7c341552c393e16f59636eb22
Patricia J. Carranza <pj.carranza4@gmail.com>
2020-07-03 16:33:23 -0700

    Create crud api

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const Plant = require("../models/plant");
 
-router.get("/plants", (req, res, next) => {});
+router.get("/plants", (req, res, next) => {
+  Plant.find({}, "name")
+    .then((data) => res.json(data))
+    .catch(next);
+});
 
-router.post("/plants", (req, res, next) => {});
+router.post("/plants", (req, res, next) => {
+  if (req.body.name) {
+    Plant.create(req.body)
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({
+      error: "The input field is empty",
+    });
+  }
+});
 
-router.delete("/plants/:id", (req, res, next) => {});
+router.delete("/plants/:id", (req, res, next) => {
+  Plant.findOneAndDelete({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch(next);
+});
 
 module.exports = router;
